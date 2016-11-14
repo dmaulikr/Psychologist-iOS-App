4f9ce0eeb7bea7821f2c97aa9b78a29ba3c55f1e
sriharsha sammeta <sriharsha4444@icloud.com>
2016-11-14 13:46:49 +0530

    Final version of Psychologist app

---
+++ Psychologist/DiagnosedHappinessViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DiagnosedHappinessViewController.swift
+//  Psychologist
+//
+//  Created by Sriharshaa Sammeta on 2/15/15.
+//  Copyright (c) 2015 Sriharshaa Sammeta. All rights reserved.
+//
+
+import UIKit
+
+class DiagnosedHappinessViewController: HappinessViewController, UIPopoverPresentationControllerDelegate{
+    
+    override var happiness: Int{
+        didSet{
+            DiagnosedHistory += [happiness]
+        }
+    }
+    
+    private let defaults = NSUserDefaults.standardUserDefaults()
+    
+    var DiagnosedHistory:[Int] {
+        get{ return defaults.objectForKey(History.DefaultsKey) as? [Int] ?? [Int]()}
+        set{ defaults.setObject(newValue, forKey: History.DefaultsKey) }
+    }
+    private struct History{
+        static let SegueIdentifier = "Show Diagnostic History"
+        static let DefaultsKey = "DiagnosedHappinessViewController.History"
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier{
+            switch identifier{
+            case History.SegueIdentifier:
+                if let tvc = segue.destinationViewController as? TextViewController{
+                    if let ppc = tvc.popoverPresentationController{
+                        ppc.delegate = self
+                    }
+                    tvc.text = "\(DiagnosedHistory)"
+                }
+            default:
+                break
+            }
+            
+        }
+    }
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.None
+    }
+}
---
+++ Psychologist/PsychologistViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PsychologistViewController.swift
+//  Psychologist
+//
+//  Created by Sriharshaa Sammeta on 2/15/15.
+//  Copyright (c) 2015 Sriharshaa Sammeta. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+
+    @IBAction func nothing(sender: UIButton) {
+        performSegueWithIdentifier("nothing", sender: nil)
+    }
+    
+    var history = ""
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var destination = segue.destinationViewController as? UIViewController
+        
+        if let navCon = destination as? UINavigationController{
+            destination = navCon.visibleViewController
+        }
+        
+        if let hvc = destination as? HappinessViewController{
+            if let identifier = segue.identifier{
+                switch identifier{
+                    case "sad":
+                        hvc.happiness = 0
+                        history += "\nsad"
+                    case "happy":
+                        hvc.happiness = 100
+                        history += "\nhappy"
+                    case "nothing":
+                        hvc.happiness = 25
+                        history += "\nnothing"
+                    default:
+                        hvc.happiness = 50
+                        if identifier == "meh"{
+                            history += "\nmeh"
+                    }
+                }
+                
+            }
+        }
+    }
+}
